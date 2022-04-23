0806046613b3c31ea0b801befe2ad216b0967027
pablenicolas <95651314+pablenicolas@users.noreply.github.com>
2022-04-22 22:46:41 -0300

    Add files via upload

---
+++ numeros_primos.js
@@ -0,0 +1,12 @@
+  for (let i = 0; i < 10; i++) {
+    switch (i) {
+        case 2: 
+        case 3:
+        case 5:
+        case 7:
+            console.log ("es numero primo"); continue
+    }
+    console.log(i)
+
+} 
+   
