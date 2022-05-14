6ac304693545d5051a2ab83e7bbf4973dd951fef
pablenicolas <95651314+pablenicolas@users.noreply.github.com>
2022-05-13 22:10:34 -0300

    Create creacion_obj.js

---
+++ JavaScript/exercise 02/creacion_obj.js
@@ -0,0 +1,14 @@
+const obj = {
+    name: "Pablo",
+    age:  27, 
+    developer: true,
+    "date of birth": new Date(1995, 0, 16),
+    "favorite book": {
+        title: "Ficciones",
+        author: "Jorge Luis Borges",
+        year: 1944,
+        url: undefined
+    }
+}
+
+console.log(obj)
