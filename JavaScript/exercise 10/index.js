454cf3c36bdc821979c0266a5eb7542deda4420a
pablenicolas <95651314+pablenicolas@users.noreply.github.com>
2022-05-18 18:34:09 -0300

    Create index.js

---
+++ JavaScript/exercise 10/index.js
@@ -0,0 +1,9 @@
+
+import * as Math from "./controller.js"
+import chalk from 'chalk';
+
+
+console.log(Math.suma(4,5))
+console.log(Math.multiplicar(1,2))
+
+console.log(chalk.green(Math.multiplicar(1,2)));
