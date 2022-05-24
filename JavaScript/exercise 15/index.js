6d486412a8d6fd5118879318b06b1074cf2ea799
pablenicolas <95651314+pablenicolas@users.noreply.github.com>
2022-05-24 13:06:18 -0300

    Create index.js

---
+++ JavaScript/exercise 15/index.js
@@ -0,0 +1,13 @@
+const nombre = "Pablo"
+const apellido = "Nuñez"
+
+const persona = {
+   nombre,
+   apellido
+}
+
+//sessionStorage.setItem("persona", JSON.stringify(persona))
+
+//localStorage.setItem("persona", JSON.stringify(persona))
+
+//document.cookie = `persona=${JSON.stringify(persona)};expires=${new Date(now.getTime() + 2 * 60000)}`
